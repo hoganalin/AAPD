0aa7681cbc39a5c41450b400e74b30fe11ced335
Datou-Wong <tou0327@gmail.com>
2025-09-06 15:15:50 +0800

    review 9/2修改

--- main.js
+++ main.js
@@ -2,3 +2,9 @@ import './assets/scss/all.scss';
 import 'bootstrap/dist/js/bootstrap.min.js';
 
 console.log('Hello world');
+
+const flipCard = document.querySelector('.flip-card');
+
+flipCard.addEventListener('click', () => {
+  flipCard.classList.toggle('flipped');
+});
